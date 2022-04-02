4533507a5052078de407be2657b9d129a4d33f0f
Patty Chen <patty.chen1017@gmail.com>
2022-04-02 01:56:02 -0400

    commit tree 519db1db575623e95ebd3b001f0c00baec218f6f

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function(){
+    return drivers.slice(0,2);
+}
+const returnLastTwoDrivers = function (){
+    return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+function createFareMultiplier(fare){
+    return function(){return fare*5};
+}
+function fareDoubler(fare){
+    return fare*2;
+}
+function fareTripler(fare){
+    return fare*3;
+}
+function selectDifferentDrivers(arrayOfDrivers, functionName){
+    if (functionName === returnFirstTwoDrivers) {return returnFirstTwoDrivers(arrayOfDrivers)}
+    else if (functionName === returnLastTwoDrivers) {return returnLastTwoDrivers(arrayOfDrivers)};
+}
